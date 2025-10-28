d15eb68a99db1ba533afc86950fa298bf024f983
CGalloway3 <cgalloway3@my.gcu.edu>
2025-10-28 16:55:33 -0500

    Add ResetBoard and IsOnBoard methods to BoardLogic calss

---
+++ Activity 2/ChessBoardClassLibrary/ChessBoardClassLibrary/Services/BusinessLogicLayer/BoardLogic.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 11/02/2020
+ * Chess Board Project
+ * Activity 2
+ * References:
+ */
+
+using ChessBoardClassLibrary.Models;
+
+namespace ChessBoardClassLibrary.Services.BusinessLogicLayer
+{
+    public class BoardLogic
+    {
+        /// <summary>
+        /// Reset the board by setting the
+        /// cell properties back to default
+        /// Encapsulate this method so it can only be
+        /// called from this class.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <returns></returns>
+        private BoardModel ResetBoard(BoardModel board)
+        {
+            // Use a foreach loop to iterate over every cell
+            foreach (CellModel cell in board.Grid)
+            {
+                // Set the properties to their defaults
+                cell.IsLegalNextMove = false;
+                cell.PieceOccupyingCell = "";
+            }
+            // Return the board back to the presentation layer
+            return board;
+        }
+
+        /// <summary>
+        /// Check if a row/column location is on the board
+        /// Encapsulate this method so it can only be
+        /// called from this class.
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        private bool IsOnBoard(BoardModel board, int row, int col)
+        {
+            // Get the size of the board
+            int size = board.Size;
+            // Check if the row is on the board
+            bool IsRowSafe = row >= 0 && row < size;
+            // Check if the column is on the board
+            bool IsColumnSafe = col >= 0 && col < size;
+            // Return true if both row and column are safe
+            return IsRowSafe && IsColumnSafe;
+        }
+
+    }
+}
