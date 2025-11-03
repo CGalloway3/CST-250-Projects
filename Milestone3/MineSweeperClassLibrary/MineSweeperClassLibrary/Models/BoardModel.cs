fa653480d12e14ce44ea7b58ba92ab3f5765c434
CGalloway3 <cgalloway3@my.gcu.edu>
2025-11-03 09:10:10 -0600

    Adding Activity 3 CountToOneRecursion to the repo

---
+++ Milestone3/MineSweeperClassLibrary/MineSweeperClassLibrary/Models/BoardModel.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 10/26/2025
+ * Mine Sweeper Class Library
+ * Milestone 1
+ * References:
+ */
+
+using MineSweeperClassLibrary.Enums;
+
+namespace MineSweeperClassLibrary.Models
+{
+    public class BoardModel
+    {
+        // Public class level variables
+        public int Size { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public CellModel[,] Cells { get; set; }
+        public int Difficulty { get; set; } = 1;
+        public int NumberOfBombs { get; set; }
+        public int RewardsRemaining { get; set; } = 0;
+        public GameState GameState { get; set; } = GameState.InProgress;
+
+        /// <summary>
+        /// Public Model constructor
+        /// </summary>
+        /// <param name="size"></param>
+        public BoardModel(int size)
+        {
+            // Initialize variables
+            Size = size;
+            Cells = new CellModel[size, size];
+
+            // Initialize the board matrix with CellModel instances
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    Cells[row, col] = new CellModel
+                    {
+                        Row = row,
+                        Column = col
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ Milestone3/MineSweeperClassLibrary/MineSweeperClassLibrary/Models/CellModel.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 10/26/2025
+ * Mine Sweeper Class Library
+ * Milestone 1
+ * References:
+ */
+
+using MineSweeperClassLibrary.Enums;
+
+namespace MineSweeperClassLibrary.Models
+{
+    public class CellModel
+    {
+        // Public class level variables
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public CellVisualStates VisualState { get; set; }
+        public bool IsVisited { get; set; }
+        public bool IsBomb { get; set; }
+        public bool IsFlagged { get; set; }
+        public int NumberOfBombNeighbors { get; set; }
+        public bool HasSpecialReward { get; set; }
+
+        /// <summary>
+        /// Public cell model constructor
+        /// </summary>
+        public CellModel()
+        {
+            Row = -1;
+            Column = -1;
+            VisualState = CellVisualStates.Hidden;
+            IsVisited = false;
+            IsBomb = false;
+            IsFlagged = false;
+            NumberOfBombNeighbors = 0;
+            HasSpecialReward = false;
+        }
+
+    }
+}
