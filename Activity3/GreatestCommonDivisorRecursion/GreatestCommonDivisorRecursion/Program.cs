14c06dfc5685d12db904af78d2dc0c6621bf103e
CGalloway3 <cgalloway3@my.gcu.edu>
2025-11-06 09:29:33 -0600

    Create a Utility class and add logic to reclusively calculate the greatest common divisor.

--- Activity3/GreatestCommonDivisorRecursion/GreatestCommonDivisorRecursion/Program.cs
+++ Activity3/GreatestCommonDivisorRecursion/GreatestCommonDivisorRecursion/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 11/09/2020
+ * Greatest Common Divisor Recursion
+ * Activity 3 Part 3
+ * References:
+ */
+
+//-----------------------------------------------
+// Start of the Main Method
+//-----------------------------------------------
+
+
+
+//-----------------------------------------------
+// End of the Main Method
+//-----------------------------------------------
+
+
+
+
+//-----------------------------------------------
+// Start of the Utility Class
+//-----------------------------------------------
+
+public class Utility
+{
+    internal static int GreatestCommonDivisor(int num1, int num2)
+    {
+        // Declare and Initialize
+        int remainder = 0;
+
+        // Base Case: num2 = 0
+        if (num1 == 0 || num2 == 0)
+        {
+            // Return the greatest common divisor
+            return num1;
+        }
+        else
+        {
+            // Get the remainder of dividing num1 and num2
+            remainder = num1 % num2;
+            // Print an update to the user
+            Console.WriteLine($"Not yet. The remainder of {num1} and {num2} is {remainder}.");
+            // Call the recursive function of the second number and the remainder
+            return GreatestCommonDivisor(num2, remainder);
+        }
+    }
+}
+
+//-----------------------------------------------
+// End of the Utility Class
+//-----------------------------------------------
