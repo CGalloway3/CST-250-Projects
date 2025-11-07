febc7d0df2170c40c5dd5609b150aa8cf2d75737
CGalloway3 <cgalloway3@my.gcu.edu>
2025-11-07 08:19:01 -0600

    Add Board and Cell Models to the project.

---
+++ Activity3/FloodFillRecursion/FloodFillRecursion/Models/BoardModel.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 11/09/2020
+ * Flood Fill Recursion
+ * Activity 3 Part 4
+ * References:
+ */
+
+namespace FloodFillRecursion.Models
+{
+    internal class BoardModel
+    {
+        // Board Model Properties
+        public int Size { get; set; }
+        public CellModel[,] Grid {get; set;}
+        public int NumShapes { get; set; }
+
+        /// <summary>
+        /// Parameterized constructor for BoardModel
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="numShapes"></param>
+        public BoardModel(int size, int numShapes)
+        {
+            // Declare and Initialize
+            Size = size;
+            NumShapes = numShapes;
+            Grid = new CellModel[Size, Size];
+
+            //Set up the Grid
+            for (int row = 0; row < Size; row++)
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    Grid[row, col] = new CellModel(row, col, "E");
+                }
+            }
+
+            // Place random shapes on the board
+            PlaceShapes();
+        }
+
+        /// <summary>
+        /// Create shape to place on the board
+        /// </summary>
+        public void PlaceShapes()
+        {
+            // Declare and Initialize
+            // Random object to generate numbers
+            Random random = new Random();
+            int shapeSize = Size / 2, row = 0, col = 0;
+
+            // Create three shapes
+            for (int shapes = 0; shapes < NumShapes; shapes++)
+            {
+                // Generate the row and col for the
+                // top left corner of the square
+                row = random.Next(0, Size - shapeSize + 1);
+                col = random.Next(0, Size - shapeSize + 1);
+                for (int offset = 0; offset < shapeSize; offset++)
+                {
+                    // Top Wall
+                    Grid[row, col + offset].Contents = "W";
+                    // Bottom Wall
+                    Grid[row + shapeSize - 1, col + offset].Contents = "W";
+                    // Left Wall
+                    Grid[row + offset, col].Contents = "W";
+                    // Right Wall
+                    Grid[row + offset, col + shapeSize - 1].Contents = "W";
+                }
+        } // End of PlaceShapes method
+    }
+}
---
+++ Activity3/FloodFillRecursion/FloodFillRecursion/Models/CellModel.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Chad Galloway
+ * CST - 250 Programming in C# II
+ * 11/09/2020
+ * Flood Fill Recursion
+ * Activity 3 Part 4
+ * References:
+ */
+
+namespace FloodFillRecursion.Models
+{
+    internal class CellModel
+    {
+        // Cell Model Properties
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public string Contents { get; set; }
+
+        /// <summary>
+        /// Parameterized constructor for CellModel
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <param name="contents"></param>
+        public CellModel(int row, int col, string contents)
+        {
+            Row = row;
+            Col = col;
+            Contents = contents;
+        }
+    }
+}
